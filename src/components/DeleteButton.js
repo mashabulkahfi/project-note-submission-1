aaa8a501fb7cfa4347d03b8c17757c532255e3a7
mashabulkahfi <ashabul.kahfi@tokopedia.com>
2022-10-02 01:06:15 +0700

    Finalize DeleteButton Components with prop validation

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { MdDelete } from 'react-icons/md';
+import PropTypes from 'prop-types';
 
 function DeleteButton({ id, onDelete }) {
   return <button className='action' onClick={() => onDelete(id)}><MdDelete /></button>
 }
+
+DeleteButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
 
 export default DeleteButton;
