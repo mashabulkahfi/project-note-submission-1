fc8eb88d8e380b5f4038e0ce4e8d639d6eb5c72a
mashabulkahfi <ashabul.kahfi@tokopedia.com>
2022-10-02 01:24:59 +0700

    Finalize NotFoundPage

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import PageNotFound from '../assets/images/PageNotFound';
 
 class NotFoundPage extends React.Component{
     render(){
         return <div>
-            {/* <img src={PageNotFound}  /> */}
             <h2>404</h2>
             <p>Page not found.</p>
             <p>
